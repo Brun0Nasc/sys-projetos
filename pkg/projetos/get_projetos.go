b209048625ce82b57897d95546d213520014a07d
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-06-23 08:36:15 -0300

    Att Get Projetos

--- pkg/projetos/get_projetos.go
+++ pkg/projetos/get_projetos.go
@@ -7,13 +7,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type BodyGetProjetos struct {
+	ID_Projeto uint `json:"id_projeto"`
+	Nome_Projeto string `json:"nome_projeto"`
+	EquipeID int `json:"equipeId"`
+}
+
 func (h handler) GetProjetos(c *gin.Context) {
 	var projetos []models.Projeto
+	body := BodyGetProjetos{}
+	var exibe []BodyGetProjetos
 
 	if result := h.DB.Find(&projetos); result.Error != nil {
 		c.AbortWithError(http.StatusNotFound, result.Error)
 		return
 	}
+
+	for _, p := range projetos {
+		body.ID_Projeto = p.ID_Projeto
+		body.Nome_Projeto = p.Nome_Projeto
+		body.EquipeID = p.EquipeID
+
+		exibe = append(exibe, body)
+	}
 	
-	c.JSON(http.StatusOK, &projetos)
+	c.JSON(http.StatusOK, &exibe)
 }
