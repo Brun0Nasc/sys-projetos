ee2de31a006fd2c59c4135fff52b432559164241
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-06-24 08:30:29 -0300

    main

---
+++ cmd/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+	"github.com/Brun0Nasc/sys-projetos/pkg/common/db"
+	"github.com/Brun0Nasc/sys-projetos/pkg/equipes"
+	"github.com/Brun0Nasc/sys-projetos/pkg/pessoas"
+	"github.com/Brun0Nasc/sys-projetos/pkg/projetos"
+	"github.com/Brun0Nasc/sys-projetos/pkg/tasks"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	viper.SetConfigFile("./pkg/common/envs/.env")
+	viper.ReadInConfig()
+
+	port := os.Getenv("PORT")
+	dbUrl := viper.Get("DB_URL").(string)
+
+	r := gin.Default()
+	h := db.Init(dbUrl)
+
+	pessoas.RegisterRoutes(r, h)
+	equipes.RegisterRoutes(r, h)
+	projetos.RegisterRoutes(r, h)
+	tasks.RegisterRoutes(r, h)
+
+	r.Run(":"+port)
+}
--- pkg/equipes/get_equipe.go
+++ pkg/equipes/get_equipe.go
@@ -9,7 +9,7 @@ import (
 
 func (h handler) GetEquipe(c *gin.Context) {
 	id := c.Param("id")
-	
+
 	var equipe models.Equipe
 
 	if result := h.DB.First(&equipe, id); result.Error != nil {
