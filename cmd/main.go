da920775381768af092cdedfa149e573a60219c5
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-06-27 08:40:28 -0300

    CORS de novo

--- cmd/main.go
+++ cmd/main.go
@@ -9,39 +9,32 @@ import (
 	"github.com/Brun0Nasc/sys-projetos/pkg/tasks"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
+	"github.com/gin-contrib/cors"
+	
 )
 
 func main() {
 	viper.SetConfigFile("./pkg/common/envs/.env")
 	viper.ReadInConfig()
-	
+
 	port := os.Getenv("PORT")
 	dbUrl := viper.Get("DB_URL").(string)
 
 	r := gin.Default()
 	h := db.Init(dbUrl)
+	
+	r.Use(cors.New(cors.Config{
+		AllowHeaders: []string{"X-Requested-With", "Content-Type", "Authorization"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowOrigins: []string{"*"},
+	}))
 
 	pessoas.RegisterRoutes(r, h)
 	equipes.RegisterRoutes(r, h)
 	projetos.RegisterRoutes(r, h)
 	tasks.RegisterRoutes(r, h)
 
-	r.Use(CORSMiddleware())
+	
 	r.Run(":" + port)
 }
 
-func CORSMiddleware() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
-
-        if c.Request.Method == "OPTIONS" {
-            c.AbortWithStatus(204)
-            return
-        }
-
-        c.Next()
-    }
-}
