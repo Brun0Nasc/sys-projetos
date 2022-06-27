b8f2beecd931a710fbfd71c1b8c5d81802efcb13
Brun0Nasc <brunonb114@gmail.com>
2022-06-27 14:12:47 -0300

    .

--- pkg/projetos/get_projeto.go
+++ pkg/projetos/get_projeto.go
@@ -4,17 +4,23 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	//"github.com/Brun0Nasc/sys-projetos/pkg/common/models"
 )
 
 func (h handler) GetProjeto(c *gin.Context) {
 	id := c.Param("id")
 
 	var projeto BodyGetProjetos
+	
 
 	if projeto := h.DB.Raw("select pr.id_projeto, pr.nome_projeto, pr.equipe_id, eq.nome_equipe from projetos as pr inner join equipes as eq on pr.equipe_id = eq.id_equipe where id_projeto = ?", id).Scan(&projeto); projeto.Error != nil {
 		c.AbortWithError(http.StatusNotFound, projeto.Error)
 		return
 	}
+
+	if err := c.BindJSON(&projeto); err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+	
 	c.JSON(http.StatusOK, &projeto)
 }
