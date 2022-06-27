59fc6bee9a36a332bfa308974b02f980c51d6c74
Brun0Nasc <brunonb114@gmail.com>
2022-06-27 15:28:45 -0300

    .

--- pkg/pessoas/get_pessoa.go
+++ pkg/pessoas/get_pessoa.go
@@ -2,26 +2,31 @@ package pessoas
 
 import (
 	"net/http"
+
+	"github.com/Brun0Nasc/sys-projetos/pkg/common/models"
 	"github.com/gin-gonic/gin"
 )
 
-type GetPessoa struct {
-	ID_Pessoa       uint   `json:"id_pessoa"`
-    Nome_Pessoa		string `json:"nome_pessoa"`
-	Funcao_Pessoa	string `json:"funcao_pessoa"`
-	EquipeID		int    `json:"equipeId"`
-	Nome_Equipe		string `json:"nome_equipe"`
-}
 
 func (h handler) GetPessoa(c *gin.Context) {
 	id := c.Param("id")
 
-	var pessoa GetPessoa
+	var pessoa models.Pessoa
+	var equipe models.Equipe
 
-	if result := h.DB.Raw("SELECT pe.id_pessoa, pe.nome_pessoa, pe.funcao_pessoa, pe.equipeId, eq.nome_equipe FROM pessoas AS pe INNER JOIN equipe AS eq on pe.equipeId = eq.id_equipe WHERE pe.id_pessoa = ?", id); result.Error != nil {
+	if result := h.DB.First(&pessoa, id); result.Error != nil {
 		c.AbortWithError(http.StatusNotFound, result.Error)
 		return
 	}
+
+	id_equipe := pessoa.EquipeID
+
+	if result := h.DB.First(&equipe, id_equipe); result.Error != nil {
+		c.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+
+	pessoa.Equipe = equipe
 
 	c.JSON(http.StatusOK, &pessoa)
 }
