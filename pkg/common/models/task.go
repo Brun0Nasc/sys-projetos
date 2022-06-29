72a64061d0aa3ca3a6322afcb925f59bf4df719b
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-06-29 08:57:05 -0300

    .

--- pkg/common/models/task.go
+++ pkg/common/models/task.go
@@ -1,7 +1,7 @@
 package models
 
 type Task struct {
-	ID_Task         uint 	`json:"id_task"`
+	ID_Task         uint 	`gorm:"primary_key" json:"id_task"`
 	Descricao_Task  string  `json:"descricao_task"`
 	PessoaID  		int		`json:"pessoa_id"`
 	ProjetoID 		int 	`json:"projeto_id"`
