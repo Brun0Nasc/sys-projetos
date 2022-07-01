4de10f0cc6e8c76080d9ff155b6531a2f9288907
Brun0Nasc <brunonb114@gmail.com>
2022-07-01 15:31:28 -0300

    Status Task Get

--- pkg/tasks/get_tasks.go
+++ pkg/tasks/get_tasks.go
@@ -8,17 +8,18 @@ import (
 )
 
 type Tasks struct {
-	ID_Task uint `json:"id_task"`
-	Descricao_Task string `json:"descricao_task"`
-	PessoaID int `json:"pessoa_id"`
-	Nome_Pessoa string `json:"nome_pessoa"`
-	ProjetoID int `json:"projeto_id"`
-	Nome_Projeto string `json:"nome_projeto"`
+	ID_Task 		uint 	`json:"id_task"`
+	Descricao_Task 	string 	`json:"descricao_task"`
+	Status 			string 	`json:"status"`
+	PessoaID 		int 	`json:"pessoa_id"`
+	Nome_Pessoa 	string 	`json:"nome_pessoa"`
+	ProjetoID 		int 	`json:"projeto_id"`
+	Nome_Projeto 	string 	`json:"nome_projeto"`
 }
 
 func (h handler) GetTasks(c *gin.Context) {
 	var tasks []Tasks
-	sql := "select tk.id_task, tk.descricao_task, tk.pessoa_id, pe.nome_pessoa, tk.projeto_id, pr.nome_projeto from tasks as tk inner join pessoas as pe on tk.pessoa_id = pe.id_pessoa inner join projetos as pr on tk.projeto_id = pr.id_projeto order by id_task"
+	sql := "select tk.id_task, tk.descricao_task, tk.status, tk.pessoa_id, pe.nome_pessoa, tk.projeto_id, pr.nome_projeto from tasks as tk inner join pessoas as pe on tk.pessoa_id = pe.id_pessoa inner join projetos as pr on tk.projeto_id = pr.id_projeto order by id_task"
 	if tasks := h.DB.Raw(sql).Scan(&tasks); tasks.Error != nil {
 		c.AbortWithError(http.StatusNotFound, tasks.Error)
 		return
