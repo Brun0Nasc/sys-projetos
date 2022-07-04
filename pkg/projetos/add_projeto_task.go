7b5f9a6aa74c44881ce98aae21367f23b41a43c7
Brun0Nasc <brunonb114@gmail.com>
2022-07-04 13:58:00 -0300

    Create add_projeto_task.go

---
+++ pkg/projetos/add_projeto_task.go
@@ -0,0 +1,44 @@
+package projetos
+
+import (
+	"net/http"
+	"strconv"
+	"github.com/Brun0Nasc/sys-projetos/pkg/common/models"
+	"github.com/gin-gonic/gin"
+)
+
+type AddTaskRequestBody struct {
+	ID_Task			uint	`json:"id_task"`
+	Descricao_Task  string 	`json:"descricao_task"`
+	PessoaID		int 	`json:"pessoa_id"`
+}
+
+func (h handler) AddTask(c *gin.Context) {
+	body := AddTaskRequestBody{}
+	id := c.Param("id")
+
+	// getting request's body
+	if err := c.BindJSON(&body); err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	var task models.Task
+	i, err := strconv.Atoi(id)
+	if err != nil {
+		panic(err)
+	}
+
+	task.ID_Task = body.ID_Task
+	task.Descricao_Task = body.Descricao_Task
+	task.PessoaID = body.PessoaID
+	task.ProjetoID = i
+	task.Status = "A fazer"
+
+	if result := h.DB.Create(&task); result.Error != nil {
+		c.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+
+	c.JSON(http.StatusCreated, &task)
+}
