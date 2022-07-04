289fa4932d1cbd46f47177e47d735367350e7100
Brun0Nasc <brunonb114@gmail.com>
2022-07-04 14:07:34 -0300

    err

--- pkg/projetos/controller.go
+++ pkg/projetos/controller.go
@@ -16,7 +16,7 @@ func RegisterRoutes(r *gin.Engine, db *gorm.DB) {
 
 	routes := r.Group("/projetos")
 	routes.POST("/", h.AddProjeto)
-	routes.POST("/:id/tasks", h.AddTaskEquipe)
+	
 	routes.GET("/", h.GetProjetos)
 	routes.GET("/:id", h.GetProjeto)
 	routes.GET("/:id/tasks", h.GetProjetoTasks)
