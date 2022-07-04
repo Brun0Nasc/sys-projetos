acb8feba28e7f9650ecd4e0b618dba2945f863f7
Brun0Nasc <brunonb114@gmail.com>
2022-07-04 14:18:53 -0300

    .

--- pkg/pessoas/get_pessoa_tasks.go
+++ pkg/pessoas/get_pessoa_tasks.go
@@ -23,7 +23,7 @@ func (h handler) GetTaskPessoa(c *gin.Context) {
 
 	var result []Result
 
-	if result := h.DB.Raw("SELECT tk.id_task pe.id_pessoa, pe.nome_pessoa, pe.funcao_pessoa, eq.nome_equipe, pr.nome_projeto, tk.descricao_task FROM pessoas AS pe INNER JOIN equipes AS eq ON pe.equipe_id = eq.id_equipe INNER JOIN projetos AS pr ON pr.equipe_id = eq.id_equipe INNER JOIN tasks as tk ON tk.projeto_id = pr.id_projeto AND tk.pessoa_id = pe.id_pessoa WHERE pe.id_pessoa = ?", id).Scan(&result); result.Error != nil {
+	if result := h.DB.Raw("SELECT tk.id_task, pe.id_pessoa, pe.nome_pessoa, pe.funcao_pessoa, eq.nome_equipe, pr.nome_projeto, tk.descricao_task FROM pessoas AS pe INNER JOIN equipes AS eq ON pe.equipe_id = eq.id_equipe INNER JOIN projetos AS pr ON pr.equipe_id = eq.id_equipe INNER JOIN tasks as tk ON tk.projeto_id = pr.id_projeto AND tk.pessoa_id = pe.id_pessoa WHERE pe.id_pessoa = ?", id).Scan(&result); result.Error != nil {
 		c.AbortWithError(http.StatusNotFound, result.Error)
 		return
 	}
