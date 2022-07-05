7ff60e4ea2d9e49db514a03239f818cd5479f8fb
Brun0Nasc <brunonb114@gmail.com>
2022-07-05 13:59:03 -0300

    .

--- pkg/tasks/add_tasks.go
+++ pkg/tasks/add_tasks.go
@@ -8,7 +8,6 @@ import (
 )
 
 type AddTaskRequestBody struct {
-	ID_Task			uint	`json:"id_task"`
 	Descricao_Task  string 	`json:"descricao_task"`
 	PessoaID		int 	`json:"pessoa_id"`
 	ProjetoID		int 	`json:"projeto_id"`
@@ -25,7 +24,6 @@ func (h handler) AddTask(c *gin.Context) {
 
 	var task models.Task
 
-	task.ID_Task = body.ID_Task
 	task.Descricao_Task = body.Descricao_Task
 	task.PessoaID = body.PessoaID
 	task.ProjetoID = body.ProjetoID
