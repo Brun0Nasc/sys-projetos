6734f8bef350305f1d57aa30993e4c721843d827
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-07-07 10:01:27 -0300

    Update add_projetos.go

--- pkg/projetos/add_projetos.go
+++ pkg/projetos/add_projetos.go
@@ -31,22 +31,34 @@ func (h handler) AddProjeto(c *gin.Context) {
 	projeto.DataInicio = dt.Format("02-01-2006")
 
 	var check int
-
+	sql1 := "select count(id_equipe) from equipes where id_equipe = ?"
 	sql := "select count(id_pessoa) from pessoas where equipe_id = ?"
 
-	if result := h.DB.Raw(sql, projeto.EquipeID).Scan(&check); result.Error != nil {
+	if result := h.DB.Raw(sql1, projeto.EquipeID).Scan(&check); result.Error != nil {
 		c.AbortWithError(http.StatusNotFound, result.Error)
 		return
 	}
 
-	if(check > 0){
+	if check == 0{
 		if result := h.DB.Create(&projeto); result.Error != nil {
 			c.AbortWithError(http.StatusNotFound, result.Error)
 			return
 		}
 		c.JSON(http.StatusCreated, &projeto)
 	} else{
-		c.JSON(http.StatusOK, gin.H{"Message":"Projetos só podem ser cadastrados em equipes que possuam membros."})
+		if result := h.DB.Raw(sql, projeto.EquipeID).Scan(&check); result.Error != nil {
+			c.AbortWithError(http.StatusNotFound, result.Error)
+			return
+		}
+	
+		if(check > 0){
+			if result := h.DB.Create(&projeto); result.Error != nil {
+				c.AbortWithError(http.StatusNotFound, result.Error)
+				return
+			}
+			c.JSON(http.StatusCreated, &projeto)
+		} else{
+			c.JSON(http.StatusOK, gin.H{"Message":"Projetos só podem ser cadastrados em equipes que possuam membros."})
+		}
 	}
-
 }
