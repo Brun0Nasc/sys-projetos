d7dd5f3f9989f6168da2c923e1fe3ae3a7158076
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-07-11 09:34:00 -0300

    Update get_projetos.go

--- pkg/projetos/get_projetos.go
+++ pkg/projetos/get_projetos.go
@@ -6,18 +6,18 @@ import (
 )
 
 type BodyGetProjetos struct {
-	ID_Projeto uint `json:"id_projeto"`
-	Nome_Projeto string `json:"nome_projeto"`
-	EquipeID int `json:"equipe_id"`
-	Nome_Equipe string `json:"nome_equipe"`
-	Status string `json:"status"`
-	DataInicio string `json:"data_inicio"`
-	DataConclusao string `json:"data_conclusao"`
+	ID_Projeto 		uint 	`json:"id_projeto"`
+	Nome_Projeto 	string 	`json:"nome_projeto"`
+	EquipeID 		int 	`json:"equipe_id"`
+	Nome_Equipe 	string 	`json:"nome_equipe"`
+	Status 			string 	`json:"status"`
+	DataInicio 		string 	`json:"data_inicio"`
+	DataConclusao 	string 	`json:"data_conclusao"`
 }
 
 func (h handler) GetProjetos(c *gin.Context) {
 	var projetos []BodyGetProjetos
-	sql := "select pr.id_projeto, pr.nome_projeto, pr.equipe_id, eq.nome_equipe, pr.status, pr.data_inicio, pr.data_conclusao from projetos as pr inner join equipes as eq on pr.equipe_id = eq.id_equipe order by id_projeto"
+	sql := "select pr.id_projeto, pr.nome_projeto, pr.equipe_id, eq.nome_equipe, pr.status, pr.data_inicio, pr.data_conclusao from projetos as pr inner join equipes as eq on pr.equipe_id = eq.id_equipe"
 
 	if projetos := h.DB.Raw(sql).Scan(&projetos); projetos.Error != nil {
 		c.AbortWithError(http.StatusNotFound, projetos.Error)
