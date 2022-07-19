b6b2d985ae3056be2c7e0892dd9205074572982c
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-07-19 11:24:24 -0300

    Update get_projetos.go

--- pkg/projetos/get_projetos.go
+++ pkg/projetos/get_projetos.go
@@ -9,10 +9,10 @@ import (
 )
 
 type Projeto struct {
-	ID_Projeto 		uint 	`gorm:"primary_key" json:"id_projeto"`
-	Nome_Projeto 	string 	`json:"nome_projeto"`
-	EquipeID 		int 	`json:"equipe_id"`
-	Status			string	`json:"status"`
+	ID_Projeto 		uint 			`gorm:"primary_key" json:"id_projeto"`
+	Nome_Projeto 	string 			`json:"nome_projeto"`
+	EquipeID 		int 			`json:"equipe_id"`
+	Status			string			`json:"status"`
 	DataInicio 		*time.Time 		`json:"data_inicio"`
 	DataConclusao 	*time.Time		`json:"data_conclusao"`
 }
