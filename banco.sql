39b6fb806e42987f78368c35238e7072e7da0c2e
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-08-10 09:53:38 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,32 +1,66 @@
-create table equipes(
-    id_equipe serial primary key not null,
-    nome_equipe varchar(50)
-)
+CREATE TABLE "equipes" (
+  "id_equipe" bigserial PRIMARY KEY,
+  "nome_equipe" varchar NOT NULL
+);
 
-create table pessoas(
-    id_pessoa serial primary key not null,
-    nome_pessoa varchar(50),
-    funcao_pessoa varchar(50),
-    equipe_id int,
-    data_contratacao date default current_date,
-    foreign key (equipe_id) references equipes(id_equipe) on delete set null on update cascade
-)
+CREATE TABLE "pessoas" (
+  "id_pessoa" bigserial PRIMARY KEY,
+  "nome_pessoa" varchar NOT NULL,
+  "funcao_pessoa" varchar NOT NULL,
+  "equipe_id" bigint,
+  "data_contratacao" timestamp NOT NULL DEFAULT (now()),
+  "favoritar" int NOT NULL DEFAULT 0
+);
 
-create table projetos(
-    id_projeto serial primary key not null, 
-    nome_projeto varchar(50) not null,
-    equipe_id int,
-    status varchar(20) default 'Em planejamento',
-    data_inicio date default current_date,
-    foreign key (equipe_id) references equipes(id_equipe) on delete set null on update cascade
-)
+CREATE TABLE "projetos" (
+  "id_projeto" bigserial PRIMARY KEY,
+  "nome_projeto" varchar NOT NULL,
+  "descricao_projeto" text NOT NULL,
+  "equipe_id" bigint NOT NULL,
+  "status" varchar NOT NULL DEFAULT 'Em planejamento',
+  "data_inicio" timestamp NOT NULL DEFAULT (now()),
+  "data_conclusao" timestamp
+);
 
-create table tasks(
-    id_task serial primary key not null,
-    descricao_task varchar(50),
-    pessoa_id int,
-    projeto_id int,
-    status varchar(20) default 'A Fazer',
-    foreign key (pessoa_id) references pessoas(id_pessoa) on delete set null on update cascade,
-    foreign key (projeto_id) references projetos(id_projeto) on delete set null on update cascade
-)
+CREATE TABLE "tasks" (
+  "id_task" bigserial PRIMARY KEY,
+  "descricao_task" text NOT NULL,
+  "pessoa_id" bigint NOT NULL,
+  "projeto_id" bigint NOT NULL,
+  "status" varchar NOT NULL DEFAULT 'Em planejamento',
+  "nivel" varchar NOT NULL
+);
+
+CREATE INDEX ON "equipes" ("id_equipe");
+
+CREATE INDEX ON "pessoas" ("id_pessoa");
+
+CREATE INDEX ON "pessoas" ("nome_pessoa");
+
+CREATE INDEX ON "pessoas" ("equipe_id");
+
+CREATE INDEX ON "projetos" ("id_projeto");
+
+CREATE INDEX ON "projetos" ("nome_projeto");
+
+CREATE INDEX ON "projetos" ("equipe_id");
+
+CREATE INDEX ON "tasks" ("id_task");
+
+CREATE INDEX ON "tasks" ("pessoa_id");
+
+CREATE INDEX ON "tasks" ("projeto_id");
+
+COMMENT ON COLUMN "pessoas"."equipe_id" IS 'pode ser nulo';
+
+COMMENT ON COLUMN "projetos"."equipe_id" IS 'um projeto tem que estar obrigatoriamente relacionado a uma equipe';
+
+COMMENT ON COLUMN "projetos"."data_conclusao" IS 'será preenchido automaticamente quando um projeto for marcado como concluído';
+
+ALTER TABLE "pessoas" ADD FOREIGN KEY ("equipe_id") REFERENCES "equipes" ("id_equipe");
+
+ALTER TABLE "projetos" ADD FOREIGN KEY ("equipe_id") REFERENCES "equipes" ("id_equipe");
+
+ALTER TABLE "tasks" ADD FOREIGN KEY ("pessoa_id") REFERENCES "pessoas" ("id_pessoa");
+
+ALTER TABLE "tasks" ADD FOREIGN KEY ("projeto_id") REFERENCES "projetos" ("id_projeto");
